72f58dd1b80ec86a876790e76b261d7b5f8b2ee2
ObayaDevOps <obayajamesdralega@gmail.com>
2021-09-08 16:02:00 +0300

    Init basic Preact PWA

---
+++ studioLabBasic/src/routes/home/index.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import style from './style.css';
+
+//Now the mission is to get p5 js working here
+const Home = () => (
+	<div class={style.home}>
+		<h1>The Dawn of the Afropocene</h1>
+		<p>studioLab!</p>
+	</div>
+);
+
+export default Home;
